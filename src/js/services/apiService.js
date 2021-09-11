f7a6183e2a0d48e5109870443db82493b2ddde91
arokokko <56476810+arokokko@users.noreply.github.com>
2021-09-11 15:20:09 +0700

    Get and serialize data with server by request

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -24,6 +24,15 @@ class Api {
             return Promise.reject(error);
         }
     }
+    async airlines() {
+        try {
+            const response = await axios.get(`${this.url}/airlines`);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+    }
     async prices(params) {
         try {
             const response = await axios.get(`${this.url}/prices/cheap`, { params, });
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,15 +5,18 @@ class Locations {
         this.api = api;
         this.countries = null;
         this.cities = null;
-        this.shortCitiesList = null;
+        this.airlines = {};
+        this.shortCitiesList = {};
     }
     async init() {
-        const response = await Promise.all([this.api.countries(), this.api.cities()]);
+        const response = await Promise.all([this.api.countries(), this.api.cities(), this.api.airlines()]);
 
-        const [countries, cities] = response;
+        const [countries, cities, airlines] = response;
         this.countries = this.serializeContries(countries);
         this.cities = this.serializeCities(cities);
-        this.shortCitiesList = this.createShortCitiesList(this.cities)
+        this.shortCitiesList = this.createShortCitiesList(this.cities);
+        this.airlines = this.serializeAirlines(airlines);
+        console.log(this.cities);
         return response;
     }
     serializeContries(countries) {
@@ -27,19 +30,42 @@ class Locations {
         return cities.reduce((acc, city) => {
             const country_name = this.getCountryNameByCode(city.country_code);
             const city_name = city.name || city.name_translation.en;
-            const key = `${city_name},${country_name}`;
-            acc[key] = city;
+            const full_name = `${city_name}, ${country_name}`;
+            acc[city.code] = {
+                ...city,
+                country_name,
+                full_name
+            }
+            return acc;
+        }, {});
+    }
+
+    serializeAirlines(airlines) {
+        return airlines.reduce((acc, item) => {
+            item.logo = `https://pics.avs.io/200/200/${item.code}.png`;
+            item.name = item.name || item.name_translations.en;
+            acc[item.code] = item;
             return acc;
         }, {});
     }
 
     getCityCodeByKey(key) {
-        return this.cities[key].code;
+        const city = Object.values(this.cities).find(item => item.full_name === key);
+        return city.code;
     }
 
+    getAirlineNameByCode(code) {
+        return this.airlines[code] ? this.airlines[code].name : '';
+    }
+
+    getAirlineLogoByCode(code) {
+        return this.airlines[code] ? this.airlines[code].logo : '';
+    }
+
+
     createShortCitiesList(cities) {
-        return Object.keys(cities).reduce((acc, key) => {
-            acc[key] = null;
+        return Object.entries(cities).reduce((acc, [, code]) => {
+            acc[code.full_name] = null;
             return acc;
         }, {});
     }
